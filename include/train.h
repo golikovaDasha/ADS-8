f498101f0bab18926886e031d982183fe559b141
Golikova Dasha <99117131+golikovaDasha@users.noreply.github.com>
2022-05-08 10:58:06 +0300

    Update train.h

--- include/train.h
+++ include/train.h
@@ -5,13 +5,13 @@
 class Train {
  private:
   struct Cage {
-    bool light; // состояние лампочки
-    Cage *next;
-    Cage *prev;
+  bool light; // состояние лампочки
+  Cage* next;
+  Cage* prev;
   };
-  int countOp; // счетчик шагов (число переходов из вагона в вагон)
-  Cage *first; // точка входа в поезд (первый вагон)
-  Cage *last;
+  int countOp,len,a; // счетчик шагов (число переходов из вагона в вагон)
+  Cage* first;
+  Cage* last;
   Train::Cage* add(bool light);
  public:
   Train();
@@ -19,4 +19,5 @@ class Train {
   int getLength();          // вычислить длину поезда
   int getOpCount();         // вернуть число переходов (из вагона в вагон)
 };
+
 #endif  // INCLUDE_TRAIN_H_
