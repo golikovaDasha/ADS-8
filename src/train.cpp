a1fce54355f056afe2e3a4f643d90e6c683cb6d9
Golikova Dasha <99117131+golikovaDasha@users.noreply.github.com>
2022-05-08 10:41:03 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -5,28 +5,27 @@ Train::Train() :countOp(0), first(nullptr), last(nullptr) {}
 int Train::getOpCount() {
   return countOp;
 }
+Train::Cage* Train::add(bool light) {
+  Cage* NewC = new Cage;
+  NewC->light = light;
+  NewC->next = nullptr;
+  NewC->prev = nullptr;
+  return NewC;
+}
 void Train::addCage(bool light) {
-if (first && last) {
-  Cage* temporary = new Cage;
-  temporary->light = light;
-  temporary->next = nullptr;
-  temporary->prev = nullptr;
-  last->next = temporary;
-  last->next->prev = last;
-  last = last->next;
+  if (first && last) {    
+    last->next = add(light);
+    last->next->prev = last;
+    last = last->next;
   if (!first->prev) {
     first->prev = last;
   }
   if (!last->next) {
     last->next = first;
   }
-  } else {
-  Cage* temporary = new Cage;
-  temporary->light = light;
-  temporary->next = nullptr;
-  temporary->prev = nullptr;  
-  first = temporary;
-  last = first;
+  } else {        
+    first = add(light);
+    last = first;
   }
 }
 int Train::getLength() {
